2fb76e90bb8ba03049928b5d6c1f76b3b3fc30db
Himemiya Taiyou <nhatminhwork1996@gmail.com>
2026-03-03 16:59:38 +0700

    Add TODO for to be config

--- src/app.py
+++ src/app.py
@@ -3,11 +3,18 @@ import uvicorn
 from fastapi import FastAPI
 from router import router as todos_router
 
+#TODO: Add config module function here to load environment variables
+
 def create_app():
     app = FastAPI()
 
+    #TODO: Middleware configuration here
+
+    #Routes configuration
     common_prefix = f"/api"
     app.include_router(todos_router, prefix=common_prefix)
+
+    #TODO Remove docs from production
 
     return app
 
